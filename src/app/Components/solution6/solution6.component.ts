9d0509e82557b1d4e2f565a0e08ea4ccacde0397
leolanese <laneseleo@gmail.com>
2024-04-18 10:30:33 +0100

    Updating solution 6

--- src/app/Components/solution6/solution6.component.ts
+++ src/app/Components/solution6/solution6.component.ts
@@ -1,14 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Signal, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import {
-  FormBuilder,
-  FormGroup,
-  FormControl,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { Observable, debounceTime, distinctUntilChanged, of, startWith, switchMap, tap } from 'rxjs';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable, debounceTime, distinctUntilChanged, of, startWith, switchMap } from 'rxjs';
 import { FilterPipe } from '../../Pipes/filter.pipe';
 import { Country } from '../../Modules/country';
 import { CountryService } from '../../Services/country.service';
@@ -22,18 +16,19 @@ import { CountryService } from '../../Services/country.service';
   template: `
     <h2>{{ title }}</h2>
     <div class="container">
-    <form [formGroup]="profileForm">
-        <input 
-            formControlName="inputSearch"
-            class="form-control" 
-            type="text" 
-            name="search" 
-            autocomplete="on" 
-            placeholder="{{ title }}"
-            aria-label="search" />
+
+      <form [formGroup]="filterForm">
+          <input 
+              formControlName="inputSearch"
+              class="form-control" 
+              type="text" 
+              name="search" 
+              autocomplete="on" 
+              placeholder="{{ title }}"
+              aria-label="search" />
 
         <ul>
-          @for(country of countries$ | async ; track country.idd){
+          @for(country of countries$ | async | filter: countryNameSignal() ; track country.idd){
             <img src="{{ country.flags.svg }}" alt="Flag of {{ country.name.official }}" class="country-flag" />
             <div class="d-flex align-items-center ms-3">
               <i class="fas fa-search me-2"></i>
@@ -42,7 +37,7 @@ import { CountryService } from '../../Services/country.service';
           }
         </ul> 
 
-      <div *ngIf="firstNameSignal()">{{ firstNameSignal() }}</div>
+      <div *ngIf="countryNameSignal()">{{ countryNameSignal() }}</div>
     </form>
     </div>
   `,
@@ -51,22 +46,22 @@ export class Solution6Component {
   title = '6- Signal + Angular Reactive forms (formControlName)';
   countries$: Observable<Country[]> = of([]);
   searchFilter: string = '';
-  searchFilterFormControl: FormControl = new FormControl('');
+
   countryService = inject(CountryService);
+
   searchFilter$!: Observable<string> 
 
-  profileForm!: FormGroup;
+  filterForm: FormGroup;
 
-  firstNameSignal: Signal<any>
+  countryNameSignal: Signal<any>;
 
- 
-  constructor(private fb: FormBuilder) {
-    this.profileForm = this.fb.group({
-      inputSearch: ['']
+   constructor() {
+    this.filterForm = new FormGroup({
+      inputSearch: new FormControl('')
     });
-
-    this.firstNameSignal = toSignal(
-      this.profileForm.get('inputSearch')?.valueChanges.pipe(debounceTime(300)) 
+  
+    this.countryNameSignal = toSignal(
+      this.filterForm.get('inputSearch')?.valueChanges.pipe(debounceTime(300)) 
         ?? of(null),
       {}
     );
@@ -74,7 +69,7 @@ export class Solution6Component {
   }
 
   ngOnInit() {
-    this.searchFilter$ =  this.profileForm.get('inputSearch')?.valueChanges.pipe(startWith('')) || of('');
+    this.searchFilter$ =  this.filterForm.get('inputSearch')?.valueChanges.pipe(startWith('')) || of('');
    
     this.countries$ = this.searchFilter$.pipe(
       debounceTime(400),
--- src/app/Pipes/filter.pipe.ts
+++ src/app/Pipes/filter.pipe.ts
@@ -8,11 +8,13 @@ import { Country } from '../Modules/country';
 })
 export class FilterPipe implements PipeTransform {
   transform(items: Country[] | null | undefined, searchText: string): Country[] {
-    if (!items) return [];
+    if (!items || !searchText) return [];
 
     searchText = searchText.toLowerCase();
 
     return items.filter(item => {
+      if (!item || !item.name || !item.name.common) return false; 
+
       console.log(`Filter pipe triggered: `, item.name.common.toLowerCase().includes(searchText));
       return item.name.common.toLowerCase().includes(searchText);
     });
